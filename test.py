Changed the cpp file to correct the interpolation. Modified the field psi, as without the - sign the simulation breaks down and the resulting field is basically 0

test.py
@@ -11,7 +11,7 @@ if __name__ == '__main__':
     
     env = KHEnv()
     model_dir = "model_folder"
-    model_name = "MLP paper_J_02.zip"
+    model_name = "MLP paper_vor_06.zip"
     model_name = os.path.join(model_dir, model_name)
     model = RecurrentPPO.load(model_name, env=env, print_system_info=True)
 
@@ -30,7 +30,7 @@ if __name__ == '__main__':
     while not term:
         plotter(env.matrix, fig, img, strat, x,cont)
         #print(np.mean(np.abs(env.vx[env.index])))
-        #action = np.random.uniform(0,1, size=(1,))#np.array([0.5])
+        #action = np.array([.5])#np.random.uniform(-3,3,(1,))
         cont.append(env.action)
         obs, reward, term, trun, info = env.step(action)
         action, state = model.predict(obs, state=state,  deterministic=True)

utilities.py
@@ -21,7 +21,7 @@ def plotter(matrix, fig, ax, ax1, x, control):
     probes_x = np.linspace(42, 86, 12, endpoint=True).astype(int)#[46, 50, 54, 58, 60, 62, 64, 66, 68, 72, 76, 80]
     index = np.ix_(probes_x, probes_z)
     X, Z = np.meshgrid(probes_x, probes_z)
-    ax.scatter(X, Z, c='yellow', marker='o', s=15, label='probes')
+    #ax.scatter(X, Z, c='yellow', marker='o', s=15, label='probes')
     ax.imshow(matrix.T,  cmap='seismic', aspect='auto')
     ax.axis('off')
     ax1.set_xdata(x[:len(control)])
@@ -39,6 +39,7 @@ def stats_printer(env, start):
     print(f"    Total iterations: {env.iteration}")
     print(f"    Epsilon: {env.eps}")
     print(f"    Phase: {env.phase}")
+    print(f"    Lateral position: {env.x0}")
     print(f"    Maximum control at time: {np.argmax(env.controls)}")
     print(f"    Maximum control: {np.max(env.controls)}")
     print(f"    Total  power control: {np.sum(env.controls)}")